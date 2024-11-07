cb5404e3f143cc65d437cf69b6ec5fb272a03be0
mahendra-wts <mahendra.v@wingsts.com>
2024-11-07 16:06:09 +0530

    Page 1 UI with cursor animation

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
-var tl = gsap.timeline()
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.2,
-  duration: 0.5,
-  delay: 0.4
-})
-tl.from(".loader-counter, .now", {
-  opacity: 0,
-  duration: 1,
-  onStart: () => {
-    var count = document.querySelector(".loader-count")
-    var grow = 0
-    setInterval(() => {
-      if (grow < 100) {
-        count.innerHTML = grow++
-      } else {
-        count.innerHTML = grow
-      }
-    }, 35)
-  }
-})
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.5,
-  delay: 3,
-  display: "none"
-})
-tl.from("#page1", {
-  y: "100%",
-  duration: 2,
-  ease: "power4.inOut"
-})
+function loadingAnimation() {
+  var tl = gsap.timeline()
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.2,
+    duration: 0.5,
+    delay: 0.4
+  })
+  tl.from(".loader-counter, .now", {
+    opacity: 0,
+    duration: 1,
+    onStart: () => {
+      var count = document.querySelector(".loader-count")
+      var grow = 0
+      setInterval(() => {
+        if (grow < 100) {
+          count.innerHTML = grow++
+        } else {
+          count.innerHTML = grow
+        }
+      }, 35)
+    }
+  })
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 3,
+    display: "none"
+  })
+  tl.from("#page1", {
+    y: "100%",
+    duration: 1.5,
+    ease: "power4.inOut"
+  })
+  tl.from("nav", {
+    opacity: 0,
+    duration: 0.5
+  })
+  tl.from(".hero-text h1, .hero-text h2", {
+    y: 150,
+    stagger: 0.2
+  })
+}
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", (e) => {
+    var cursor = document.querySelector(".cursor")
+    console.log("mousemove", e.x, e.y)
+    gsap.to(".cursor", {
+      x: e.x,
+      y: e.y
+      // duration: 0.1
+    })
+  })
+
+  Shery.makeMagnet(".nav-menu li, .logo", {
+    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    // duration: 0.5
+  })
+}
+
+loadingAnimation()
+
+cursorAnimation()
